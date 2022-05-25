700c6691b91f280b4b36df36ae940f9c3fbfbc16
Alfadrizki Vivananda <105194382+Alfadrizki@users.noreply.github.com>
2022-05-25 13:25:01 +0700

    Add files via upload

---
+++ Hello/Hello.java
@@ -0,0 +1,10 @@
+package com.tutorial.Hello;
+
+public class Hello {
+    public static void main (String[]args) {
+        System.out.println("Hello nama saya Alfa");
+    }
+        
+    
+    
+}
